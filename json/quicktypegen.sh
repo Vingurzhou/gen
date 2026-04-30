4c1cc20c02eb1a18a543c7f5f5aa87ec793e1659
周文喆 <zwz0123460218@icloud.com>
2026-04-30 12:25:29 +0800

    feat: add gsio JSON structure and update quicktype generation scripts

--- json/quicktypegen.sh
+++ json/quicktypegen.sh
@@ -8,4 +8,14 @@ quicktype \
   --code-format with-struct \
   --namespace qz
 
-quicktype json/qz --out gen/qz.json.py --lang python
+quicktype json/qz --out gen/qz_json.py --lang python
+#############################################################
+quicktype \
+  json/gsio \
+  --lang cpp \
+  --out gen/gsio.json.h \
+  --no-boost \
+  --code-format with-struct \
+  --namespace gsio
+
+quicktype json/gsio --out gen/giso_json.py --lang python
